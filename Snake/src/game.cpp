2908099b3d576cfdb65497cd024ec27a269bb80a
Пеньков Максим Олександрович <ipz231_pmo@student.ztu.edu.ua>
2025-02-03 20:46:06 +0200

    game::update minimize

--- Snake/src/game.cpp
+++ Snake/src/game.cpp
@@ -16,20 +16,24 @@ void game::update(double deltaTime)
 	if (gameOver) return;
 	moveProcess += deltaTime;
 
-	if (moveProcess > MOVE_TIME)
-	{
-		moveProcess -= MOVE_TIME;
-		if (dir == Up) head.y--;
-		if (dir == Left) head.x--;
-		if (dir == Down) head.y++;
-		if (dir == Right) head.x++;
+	priv::move();
 
-		if (head.x < 1) head.x = 1;
-		if (head.y < 1) head.y = 1;
-		if (head.x > WIDTH-2) head.x = WIDTH - 2;
-		if (head.y > HEIGHT-2) head.y = HEIGHT - 2;
-	}
+}
 
+void game::priv::move()
+{
+	if (moveProcess > MOVE_TIME) return;
+	
+	moveProcess -= MOVE_TIME;
+	if (dir == Up) head.y--;
+	if (dir == Left) head.x--;
+	if (dir == Down) head.y++;
+	if (dir == Right) head.x++;
+
+	if (head.x < 1) head.x = 1;
+	if (head.y < 1) head.y = 1;
+	if (head.x > WIDTH - 2) head.x = WIDTH - 2;
+	if (head.y > HEIGHT - 2) head.y = HEIGHT - 2;
 }
 
 Direction game::dir;
